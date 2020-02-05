84dacf1407ba647a8accce1a902f11b1ddc1232b
Justin Pierce <jwpierce1984@gmail.com>
2020-02-05 19:55:31 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,3 +1,18 @@
 module Dance
   
+  def twirl
+    "I'm twirling!"
+  end
+  
+  def jump
+    "Look how high I'm jumping!"
+  end
+  
+  def pirouette
+    "I'm doing a pirouette"
+  end
+  
+  
+  
+  
 end
